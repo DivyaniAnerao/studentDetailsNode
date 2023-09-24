2ad3ef7a614696e066d125df01b73c1b294bc394
unknown <divyanianerao97@gmail.com>
2023-09-24 23:12:50 +0530

    initial commit: backend ready

---
+++ app.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const express = require("express");
+const Studentsmodel = require("./mongooseConnect");
+const addData = require("./headersMiddleware");
+const app = express();
+const PORT = 3000;
+//const PORT = process.env.PORT | 3000;
+app.use(express.json());
+
+app.get("/students/details", addData, async (req, res) => {
+  let students = {};
+
+  const id = req.query.id;
+  const dates = req.query.dates;
+  console.log(id);
+  if (dates != 0 && id != 0) {
+    let dateFrom = new Date(dates);
+    let nextDay = new Date(dateFrom);
+    nextDay.setDate(dateFrom.getDate() + 1);
+    console.log(dateFrom);
+    console.log(nextDay);
+    students = await Studentsmodel.find({
+      UserId: id,
+      SubmitDateTime: { $gte: dateFrom, $lt: nextDay },
+    });
+  } else if (id == 0 && dates == 0) {
+    students = await Studentsmodel.find();
+  } else if (id == 0 && dates) {
+    let dateFrom = new Date(dates);
+    let nextDay = new Date(dateFrom);
+    nextDay.setDate(dateFrom.getDate() + 1);
+    console.log(dateFrom);
+    console.log(nextDay);
+    students = await Studentsmodel.find({
+      SubmitDateTime: { $gte: dateFrom, $lt: nextDay },
+    });
+  } else if (id && dates == 0) {
+    students = await Studentsmodel.find({ UserId: id });
+  }
+  res.send(students);
+});
+
+app.get("/students", addData, async (req, res) => {
+  let students = {};
+  if (req.query.dates) {
+  } else {
+    students = await Studentsmodel.distinct("UserId");
+  }
+  res.send(students);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...!`);
+});
---
+++ mongooseConnect.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const mongoUrl = "mongodb://127.0.0.1:27017/snappet";
+//const mongoUrl = process.env.MONGO_URL | "mongodb://127.0.0.1:27017/snappet";
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to database...!");
+  });
+
+const studentSchema = new mongoose.Schema({
+  SubmittedAnswerId: {
+    type: Number,
+    default: null,
+  },
+  SubmitDateTime: Date,
+  Correct: {
+    type: Number,
+    default: null,
+  },
+  Progress: {
+    type: Number,
+    default: null,
+  },
+  UserId: {
+    type: Number,
+    default: null,
+  },
+  ExerciseId: Number,
+  Difficulty: {
+    type: String,
+    default: null,
+  },
+  Subject: String,
+  Domain: String,
+  LearningObjective: String,
+});
+
+const Studentsmodel = new mongoose.model("student", studentSchema);
+module.exports = Studentsmodel;
