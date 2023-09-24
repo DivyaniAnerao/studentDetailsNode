c7e8d218b544c95e5b93e238988493b4c57be43b
unknown <divyanianerao97@gmail.com>
2023-09-25 01:57:51 +0530

    script to insert the data to database is removed

--- app.js
+++ app.js
@@ -2,47 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const Studentsmodel = require("./mongooseConnect");
 const addData = require("./headersMiddleware");
-const fs = require('fs');
-const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 //const PORT = process.env.PORT | 3000;
 app.use(express.json());
-const func = async()=>{
-const data = fs.readFileSync('./work.json', 'utf8');
 
-    let fileData1= JSON.parse(data);
-    //console.log(fileData1);
-
-    fileData1.map(async(fileData)=>{
-        //let fileData = fileData1[0];
-        let dateTemp = fileData.SubmitDateTime.replaceAll(/\s/g,'');
-        let dateFor = new Date(dateTemp);
-          const data1 = new Studentsmodel(
-            {
-              "SubmittedAnswerId":fileData.SubmittedAnswerId,
-              "SubmitDateTime":dateFor,
-              "Correct":fileData.Correct,
-              "Progress":fileData.Progress,
-              "UserId":fileData.UserId,
-              "ExerciseId":fileData.ExerciseId,
-              "Difficulty":fileData.Difficulty,
-              "Subject":fileData.Subject,
-              "Domain":fileData.Domain,
-              "LearningObjective":fileData.LearningObjective
-            })
-            
-            console.log(data1)
-           // mongoose.set('debug',true);
-            /*let result =  await data1.save();
-
-            mongoose.set('debug',(cal,method,query,doc)=>{
-                console.log(JSON.stringify(query))
-                    })
-            console.log(result)*/
-    })
-}
-func();
 app.get("/students/details", addData, async (req, res) => {
   let students = {};
 
