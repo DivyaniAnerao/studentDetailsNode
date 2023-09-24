ee7996c4351a6a43ad60ade4745143fdfba23f12
unknown <divyanianerao97@gmail.com>
2023-09-25 03:25:20 +0530

    script to insert the data to database is removed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 //const Studentsmodel = require("./mongooseConnect");
 const addData = require("./headersMiddleware");
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 //const PORT = process.env.PORT | 3000;
 app.use(express.json());
 app.get("/students/details/test", addData, async (req, res) => {
